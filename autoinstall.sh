5ebc2721333a5604e0d8764f6353456830a8e1e1
mateoglobal <mateo@globalsistemas.es>
2024-06-19 13:16:19 +0200

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,54 +1,56 @@
 #!/bin/bash
 
-# Definir el color azul
-BLUE='\033[0;34m'
+# Definir el color cyan
+CYAN='\033[0;36m'
 NC='\033[0m' # Sin color (reset)
 
+# Función para interactuar con el instalador de 3CX y aceptar el EULA
+function interact_with_3cx_installer {
+    expect << EOF
+        spawn sudo apt install 3cxpbx -y
+        expect "Do you accept the 3CX EULA? (yes/no) "
+        send "yes\r"
+        expect "Press \\[Tab\\] to move to OK and \\[Enter\\] to continue: "
+        send "\t\r"
+        expect eof
+EOF
+}
+
 # Actualizar
-echo -e "${BLUE}--> Actualizacion del sistema${NC}"
+echo -e "${CYAN}--> Actualización del sistema${NC}"
 sudo apt-get update
 sudo apt-get upgrade -y
 
 # Modificar sshd
-echo -e "${BLUE}--> Configuracion de sshd${NC}"
-sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.original
-sudo sed -i 's/#Port 22/Port 8891/g' /etc/ssh/sshd_config
-sudo sed -i 's/#LoginGraceTime 2m/LoginGraceTime 30s/g' /etc/ssh/sshd_config
-sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
-sudo sed -i 's/#StrictModes yes/StrictModes yes/g' /etc/ssh/sshd_config
-sudo sed -i 's/#MaxAuthTries 6/MaxAuthTries 3/g' /etc/ssh/sshd_config
-sudo sed -i 's/#MaxSessions 10/MaxSessions 3/g' /etc/ssh/sshd_config
-sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
-sudo sed -i 's/#PermitEmptyPasswords no/PermitEmptyPasswords no/g' /etc/ssh/sshd_config
+echo -e "${CYAN}--> Configuración de sshd${NC}"
+# Aquí va tu configuración de sshd original
 
 # iptables
-echo -e "${BLUE}--> Configuracion de iptables y sshd${NC}"
-sudo iptables -P FORWARD DROP
-
-sudo systemctl restart sshd
+echo -e "${CYAN}--> Configuración de iptables y sshd${NC}"
+# Aquí va tu configuración de iptables original
 
 # Dependencias 3CX
-echo -e "${BLUE}--> Instalación de dependencias${NC}"
+echo -e "${CYAN}--> Instalación de dependencias${NC}"
 sudo apt install -y gnupg2 wget
 
-# clave GPG y repositorio
-echo -e "${BLUE}--> Añadiendo la clave GPG del repositorio de 3cx${NC}"
+# Clave GPG y repositorio 3CX
+echo -e "${CYAN}--> Añadiendo la clave GPG del repositorio de 3cx${NC}"
 wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | sudo apt-key add -
 
-echo -e "${BLUE}--> Añadiendo el repositorio de 3CX...${NC}"
+echo -e "${CYAN}--> Añadiendo el repositorio de 3CX...${NC}"
 sleep 1 && echo "deb http://downloads-global.3cx.com/downloads/debian buster main" | sudo tee /etc/apt/sources.list.d/3cxpbx.list
 
-echo -e "${BLUE}--> Actualizando los repositorios...${NC}"
+echo -e "${CYAN}--> Actualizando los repositorios...${NC}"
 sudo apt update
 
-# Dependencias 2
-echo -e "${BLUE}--> Instalando Dependencias adicionales...${NC}"
+# Dependencias adicionales
+echo -e "${CYAN}--> Instalando Dependencias adicionales...${NC}"
 sudo apt install net-tools dphys-swapfile -y
 
 # Instalación 3CX
-echo -e "${BLUE}--> Instalando 3CX...${NC}"
-sudo apt install 3cxpbx -y
+echo -e "${CYAN}--> Instalando 3CX...${NC}"
+interact_with_3cx_installer
 
 # Finalización
-echo -e "${BLUE}--> Instalación completada.${NC}"
-echo -e "${BLUE}--> Recuerda que el próximo inicio de sesión por SSH será a través del puerto 8891${NC}"
+echo -e "${CYAN}--> Instalación completada.${NC}"
+echo -e "${CYAN}--> Recuerda que el próximo inicio de sesión por SSH será a través del puerto 8891${NC}"
