0c2a69d31f5848f5a3161eb2c3bccd7fde5e3f58
mateoglobal <mateo@globalsistemas.es>
2024-06-19 12:26:50 +0200

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Definir el color azul
+BLUE='\033[0;34m'
+NC='\033[0m' # Sin color (reset)
+
 # Actualizar
-echo "Actualizacion del sistema"
+echo -e "${BLUE}--> Actualizacion del sistema${NC}"
 sudo apt-get update
 sudo apt-get upgrade -y
 
 # Modificar sshd
-echo "Configuracion de sshd"
+echo -e "${BLUE}--> Configuracion de sshd${NC}"
 sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.original
 sudo sed -i 's/#Port 22/Port 8891/g' /etc/ssh/sshd_config
 sudo sed -i 's/#LoginGraceTime 2m/LoginGraceTime 30s/g' /etc/ssh/sshd_config
@@ -18,33 +22,33 @@ sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh
 sudo sed -i 's/#PermitEmptyPasswords no/PermitEmptyPasswords no/g' /etc/ssh/sshd_config
 
 # iptables
-echo "Configuracion de iptables y sshd"
+echo -e "${BLUE}--> Configuracion de iptables y sshd${NC}"
 sudo iptables -P FORWARD DROP
 
 sudo systemctl restart sshd
 
 # Dependencias 3CX
-echo "Instalación de dependencias"
+echo -e "${BLUE}--> Instalación de dependencias${NC}"
 sudo apt install -y gnupg2 wget
 
 # clave GPG y repositorio
-echo "Añadiendo la clave GPG del repositorio de 3cx"
+echo -e "${BLUE}--> Añadiendo la clave GPG del repositorio de 3cx${NC}"
 wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | sudo apt-key add -
 
-echo "Añadiendo el repositorio de 3CX..."
+echo -e "${BLUE}--> Añadiendo el repositorio de 3CX...${NC}"
 sleep 1 && echo "deb http://downloads-global.3cx.com/downloads/debian buster main" | sudo tee /etc/apt/sources.list.d/3cxpbx.list
 
-echo "Actualizando los repositorios..."
+echo -e "${BLUE}--> Actualizando los repositorios...${NC}"
 sudo apt update
 
 # Dependencias 2
-echo "Instalando Dependencias adicionales..."
+echo -e "${BLUE}--> Instalando Dependencias adicionales...${NC}"
 sudo apt install net-tools dphys-swapfile -y
 
 # Instalación 3CX
-echo "Instalando 3CX..."
+echo -e "${BLUE}--> Instalando 3CX...${NC}"
 sudo apt install 3cxpbx -y
 
 # Finalización
-echo "Instalación completada."
-echo "Recuerda que el próximo inicio de sesión por SSH será a través del puerto 8891"
+echo -e "${BLUE}--> Instalación completada.${NC}"
+echo -e "${BLUE}--> Recuerda que el próximo inicio de sesión por SSH será a través del puerto 8891${NC}"
