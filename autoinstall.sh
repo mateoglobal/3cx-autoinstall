2bb42747c439a1c84100ed681ff12ff1b50a2349
mateoglobal <mateo@globalsistemas.es>
2024-06-20 11:30:18 +0200

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -7,41 +7,41 @@ NC='\033[0m' # Sin color (reset)
 
 # Actualizar
 echo -e "${CYAN}--> Actualización del sistema${NC}"
-sleep 4
+sleep 2
 sudo apt-get update
 sudo apt-get upgrade -y
 
 # Modificar sshd
 echo -e "${CYAN}--> Configuración de sshd${NC}"
-sleep 4
+sleep 2
 # Aquí va tu configuración de sshd original
 
 # iptables
 echo -e "${CYAN}--> Configuración de iptables y sshd${NC}"
-sleep 4
+sleep 2
 # Aquí va tu configuración de iptables original
 
 # Dependencias 3CX
 echo -e "${CYAN}--> Instalación de dependencias${NC}"
-sleep 4
+sleep 2
 sudo apt install -y gnupg2 wget
 
 # Clave GPG y repositorio 3CX
 echo -e "${CYAN}--> Añadiendo la clave GPG del repositorio de 3cx${NC}"
-sleep 4
+sleep 2
 wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | sudo apt-key add -
 
 echo -e "${CYAN}--> Añadiendo el repositorio de 3CX...${NC}"
-sleep 4
+sleep 2
 sleep 1 && echo "deb http://downloads-global.3cx.com/downloads/debian buster main" | sudo tee /etc/apt/sources.list.d/3cxpbx.list
 
 echo -e "${CYAN}--> Actualizando los repositorios...${NC}"
-sleep 4
+sleep 2
 sudo apt update
 
 # Dependencias adicionales
 echo -e "${CYAN}--> Instalando Dependencias adicionales...${NC}"
-sleep 4
+sleep 2
 sudo apt install net-tools dphys-swapfile -y
 
 # Aviso ssh
